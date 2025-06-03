44c79559a94441173e6e2688f7c67fa72e8595e3
Tom <tom@Toms-MacBook-Pro.local>
2025-06-03 17:36:27 +0800

    chore: remove Facebook Pixel from site

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,23 +66,6 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/images/idevibelogo.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/idevibelogo.png" />
         <link rel="manifest" href="/manifest.json" />
-        {/* Facebook Pixel Code */}
-        <script dangerouslySetInnerHTML={{ __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1030650302523455');
-          fbq('track', 'PageView');
-        `}} />
-        <noscript>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=1030650302523455&ev=PageView&noscript=1" />
-        </noscript>
         <Script src="/buybox.js" strategy="afterInteractive" />
       </head>
       <body className={`${inter.className} bg-black bg-dotted-grid`}>{children}</body>
