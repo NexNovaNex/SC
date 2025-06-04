5370e1e6b57ae7934bcacc475b836084f77235ad
Tom <tom@Toms-MacBook-Pro.local>
2025-06-04 11:16:25 +0800

    feat: add Facebook Pixel (ID: 1402748160878093) base code and noscript fallback to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,9 +66,31 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/images/idevibelogo.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/idevibelogo.png" />
         <link rel="manifest" href="/manifest.json" />
-        <Script src="/buybox.js" strategy="afterInteractive" />
+        {/* Facebook Pixel Base Code */}
+        <Script id="fb-pixel-base" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1402748160878093');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* End Facebook Pixel */}
       </head>
-      <body className={`${inter.className} bg-black bg-dotted-grid`}>{children}</body>
+      <body className={`${inter.className} bg-black bg-dotted-grid`}>
+        {/* Facebook Pixel NoScript Fallback */}
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=1402748160878093&ev=PageView&noscript=1" />
+        </noscript>
+        {/* End Facebook Pixel NoScript */}
+        {children}
+      </body>
     </html>
   );
 }
